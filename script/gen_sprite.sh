74d519d482b33ba92cf087971d763972769501ac
Dmitry Golovin <dima@golovin.in>
2018-01-27 18:24:04 +0200

    add grabbing corpse sprite

--- script/gen_sprite.sh
+++ script/gen_sprite.sh
@@ -40,6 +40,25 @@ single_person() {
   done
 }
 
+two_persons() {
+  name=$1
+  j=96 # magic
+
+  for color_a in $colors
+  do
+    j=$((j+1))
+    k=96 # magic
+    sed -i "0,/fill:#[0-9a-f]\+;/{s/fill:#[0-9a-f]\+;/fill:#$color_a;/}" "$name.svg"
+    for color_b in $colors
+    do
+      k=$((k+1))
+      filename=$(/usr/bin/printf "$name-\x$(printf %x $j)-\x$(printf %x $k).png")
+      sed -i "72,/fill:#[0-9a-f]\+;/{s/fill:#[0-9a-f]\+;/fill:#$color_b;/}" "$name.svg"
+      generate_svg "$name.svg" $filename
+    done
+  done
+}
+
 simple_sprite "faucet"
 simple_sprite "alien"
 simple_sprite "doctor"
@@ -48,3 +67,4 @@ single_person "npc"
 single_person "dead"
 single_person "mask"
 single_person "gasmask"
+two_persons "grabbingdead"
